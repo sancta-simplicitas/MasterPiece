9dc4742e0daacbdef0161c39395538a944b8a254
hzqd <40200761+hzqd@users.noreply.github.com>
2020-03-04 20:31:06 +0800

    Update sumCalc.rs

--- Rust/sumCalc.rs
+++ Rust/sumCalc.rs
@@ -1,18 +1,27 @@
-trait Let {
+use std::io::stdin;
+
+trait KtStd {
     fn let_imut<R, F>(&self, block: F) -> R where F: FnOnce(&Self) -> R;
+
+    fn also_mut<F>(&mut self, block: F) -> &mut Self where F: FnOnce(&mut Self);
 }
 
-impl <T> Let for T {
+impl<T> KtStd for T {
     fn let_imut<R, F>(&self, block: F) -> R where F: FnOnce(&Self) -> R {
         block(self)
+    }
+
+    fn also_mut<F>(&mut self, block: F) -> &mut Self where F: FnOnce(&mut Self) {
+        block(self);
+        self
     }
 }
 
 fn main() {
     loop {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).unwrap();
-        s.split_whitespace()
+        String::new()
+            .also_mut(|mut s| { stdin().read_line(&mut s).unwrap(); })
+            .split_whitespace()
             .filter_map(|c| c.parse::<f64>().ok())
             .sum::<f64>()
             .let_imut(|n| println!("{}", n))
